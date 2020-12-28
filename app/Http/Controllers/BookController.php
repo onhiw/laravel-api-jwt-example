08ff974ff10c60a0d2f239c94c2eb7141d19dfaf
Ronny Rusmayadi <ronirusmayadi4@gmail.com>
2020-12-28 14:45:40 +0700

    laravel api jwt

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class BookController extends Controller
+{
+    public function book() {
+        $data = "Data All Book";
+        return response()->json($data, 200);
+    }
+
+    public function bookAuth() {
+        $data = "Welcome " . Auth::user()->name;
+        return response()->json($data, 200);
+    }
+}
